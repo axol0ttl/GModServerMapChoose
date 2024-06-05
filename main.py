0dfcafcadcf8226b60db499bb75d056b7bae87a3
axxxo <holodoff.senya@yandex.ru>
2024-06-05 23:04:43 +0300

    beautify

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 
 
 def Start(set, clientpath):
-    n=4
+    n = 4
     maplist = os.listdir(set)
     maplist = ([item[:-n] for item in maplist])
     print(maplist)
     choose = int(input(''))
-    selectedmap = maplist[choose-1]
+    selectedmap = maplist[choose - 1]
     print(selectedmap)
     os.chdir(clientpath)
-    os.system(f'start srcds.exe +maxplayers 5 -console +host_workshop_collection 2571938184 +gamemode sandbox +map {selectedmap}  +r_hunkalloclightmaps 0')
+    os.system(
+        f'start srcds.exe +maxplayers 5 -console +host_workshop_collection 2571938184 +gamemode sandbox +map {selectedmap}  +r_hunkalloclightmaps 0')
 
 
 pathserver = input('Enter the path to your server  \nIt is like:  *****\steamapps\common\GarrysModDS\n')
