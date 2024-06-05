cacbf3d3d304b0a441b7012d50d0117cbf3d2385
axxxo <holodoff.senya@yandex.ru>
2024-06-05 22:43:56 +0300

    it's now finally working, but im planning to add saving path feature in future

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import os
-selectedMap = 'gm_construct'
-#пиздец мне лень думать
-def maplistget(directoryd):
-    futurePurpose = directoryd
-    mapfolder = directoryd + '\garrysmod\downloadlists'
-    directorylist = os.listdir(mapfolder)
-    n = 4
-    maplist = ([item[:-n] for item in directorylist])
+
+
+def Start(set, clientpath):
+    n=4
+    maplist = os.listdir(set)
+    maplist = ([item[:-n] for item in maplist])
     print(maplist)
     choose = int(input(''))
-    selectedMap = maplist[choose]
-    startserver(futurePurpose)
-    return selectedMap
-
-def startserver(folder):
-    print(List)
-    print(maplistget)
-    os.chdir(folder)
-    os.system(
-        f'start srcds.exe +maxplayers 5 -console +host_workshop_collection 2571938184 +gamemode sandbox +map {List} +r_hunkalloclightmaps 0')
+    selectedmap = maplist[choose-1]
+    print(selectedmap)
+    os.chdir(clientpath)
+    os.system(f'start srcds.exe +maxplayers 5 -console +host_workshop_collection 2571938184 +gamemode sandbox +map {selectedmap}  +r_hunkalloclightmaps 0')
 
 
-List = maplistget(input('Enter the directory of your server \nIt is like: *****\steamapps\common\GarrysModDS\n'))
+pathserver = input('Enter the path to your server  \nIt is like:  *****\steamapps\common\GarrysModDS\n')
+path = pathserver + '\garrysmod\downloadlists'
+a = Start(path, pathserver)
